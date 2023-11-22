29f563e0d42793e9aea9d64eaa3ed48a96cd7c79
theKunalSingh143 <120199298+theKunalSingh143@users.noreply.github.com>
2023-11-22 10:39:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """
 Apache License 2.0
-Copyright (c) 2022 @PYRO_BOTZ
+Copyright (c) 2022 @IllegalDeveloperBot
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -15,9 +15,9 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-Telegram Link : https://t.me/PYRO_BOTZ 
-Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
-License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
+Telegram Link : https://t.me/IllegalDeveloperBot
+Repo Link : https://github.com/IllegalDevelopers/ILLEGAL-RENAME-BOT
+License Link : https://github.com/IllegalDevelopers/ILLEGAL-RENAME-BOT/blob/main/LICENSE
 """
 
 import re, os, time
