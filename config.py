58b783a8c5c5ae0a4827b6d8ae2e502c22e70ff5
theKunalSingh143 <120199298+theKunalSingh143@users.noreply.github.com>
2023-11-24 21:53:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/5ba316e179b39faa71904.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1229852181').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001848348787") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "Illegal_Developer") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
     # wes response configuration     
