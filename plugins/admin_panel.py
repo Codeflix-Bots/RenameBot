919545998f97c0d1ce7ea4336b6e0150abe588f2
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2024-02-03 20:03:26 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -128,6 +128,8 @@ async def ban_a_user(bot, message):
         await db.ban_user(k.id, reason)
         temp.BANNED_USERS.append(k.id)
         await message.reply(f"Successfully banned {k.mention}")
+        await bot.send_message(k.id, f"**Hello 👋 {k.mention}\n\nNow you are banned by my admin**")
+        
 
 
     
@@ -155,9 +157,7 @@ async def unban_a_user(bot, message):
     except Exception as e:
         return await message.reply(f'Error - {e}')
     else:
-        jar = await db.get_ban_status(k.id)
-        if not jar['is_banned']:
-            return await message.reply(f"{k.mention} is not yet banned.")
         await db.remove_ban(k.id)
         temp.BANNED_USERS.remove(k.id)
         await message.reply(f"Successfully unbanned {k.mention}")
+        await bot.send_message(k.id, f"**Hello 👋 {k.mention}\n\nNow you are unbanned by my admin**")
